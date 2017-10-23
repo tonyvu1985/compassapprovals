b4936d676300be0809d203fe19940109b5f0b238
Tony Vu <tony.vu@temando.com>
2017-10-23 11:07:06 +1100

    footer changed

--- wp-content/themes/thearchitect-wpl/footer.php
+++ wp-content/themes/thearchitect-wpl/footer.php
@@ -20,13 +20,18 @@
 			</footer>
 
 		</div><!-- /#container -->
-
+                <div id="member-logos">
+                    <img src="http://www.compassapprovals.com.au/wp-content/uploads/2017/10/rs-hia-member.jpg" alt="rs hia member" />
+                    <img src="http://www.compassapprovals.com.au/wp-content/uploads/2017/10/master-builder-ass.jpg" alt="master builder ass" />
+                    <img src="http://www.compassapprovals.com.au/wp-content/uploads/2017/10/building-professionals-board.jpg" alt="building professionals board" />
+                    <img src="http://www.compassapprovals.com.au/wp-content/uploads/2017/10/aibs.jpg" alt="aibs" />
+                </div>
 		<div class="copy">
 			<?php if ( ot_get_option('wpl_copyright') ) { 
 				echo ot_get_option('wpl_copyright'); 
 			} ?>
 
-			<?php _e('Designed by', 'thearchitect-wpl'); ?> <a href="https://wplook.com/theme/architect-wordpress-theme/<?php if (ot_get_option('wpl_affiliate')) { echo "?ref="; echo ot_get_option('wpl_affiliate'); } else { echo "?utm_source=Footer-URL&utm_medium=link&utm_campaign=The-Architect";} ?>" title="<?php _e('WPlook', 'thearchitect-wpl'); ?>" target="_blank">WPlook Studio</a>
+			<!--<?php _e('Designed by', 'thearchitect-wpl'); ?> <a href="https://wplook.com/theme/architect-wordpress-theme/<?php if (ot_get_option('wpl_affiliate')) { echo "?ref="; echo ot_get_option('wpl_affiliate'); } else { echo "?utm_source=Footer-URL&utm_medium=link&utm_campaign=The-Architect";} ?>" title="<?php _e('WPlook', 'thearchitect-wpl'); ?>" target="_blank">WPlook Studio</a> -->
 		</div>
 
 		<?php if ( ot_get_option('wpl_google_analytics_tracking_code') ) {
